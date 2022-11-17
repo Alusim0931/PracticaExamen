7aade05eeef8cde85d7fbc622945cca4da07df9a
rsn <alusim0931@ieselcaminas.org>
2022-11-17 11:15:41 +0100

    Initial commit

---
+++ src/Rectangulo3.java
@@ -0,0 +1,16 @@
+public class Rectangulo3 {
+    private double lado1;
+    private double lado2;
+    public Rectangulo3 (double lado1, double lado2){
+        lado1=5;
+        lado2=5;
+    }
+    public double obtenerArea(){
+        return 5*5;
+
+    }
+    public double obtenerPerimetro(){
+        return (5*2)+(5*2);
+    }
+}
+
